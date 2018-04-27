f1875d035f5bc79b48ab0cbda86896944b69f8e2
renatoadsumus <renatoadsumus@gmail.com>
2018-04-27 15:54:37 -0300

    Iniciando o container do Mongo

--- site/profile/manifests/docker_container_onpremise.pp
+++ site/profile/manifests/docker_container_onpremise.pp
@@ -9,6 +9,11 @@ class profile::docker_container_onpremise inherits profile::base{
 		image   => 'renatoadsumus/gocd:latest',
 		ports   => ['8153:8153','8154:8154'],
 		require => Docker::Image['renatoadsumus/gocd'],
+		
+	::docker::run { 'mongodb':
+		image   => 'renatoadsumus/mongodb:latest',
+		ports   => ['2717:2717'],
+		require => Docker::Image['renatoadsumus/mongodb'],
   }  
  
 }
