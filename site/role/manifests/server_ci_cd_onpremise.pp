213157d36d9fe2231019bb07bb2a9a2374e57113
renatoadsumus <renatoadsumus@gmail.com>
2018-04-30 17:37:00 -0300

    Alterando caminho defaul docker para OPT

--- site/role/manifests/server_ci_cd_onpremise.pp
+++ site/role/manifests/server_ci_cd_onpremise.pp
@@ -8,13 +8,14 @@ class role::server_ci_cd_onpremise {
         ensure => 'directory',					
 	}
 	
+	#https://forums.docker.com/t/no-route-to-host-network-request-from-container-to-host-ip-port-published-from-other-container/39063/5
 	file_line {"/etc/firewalld/zones/public_copia.xml":
 		path => "/etc/firewalld/zones/public_copia.xml",
 		line => '<rule family="ipv4"><source address="172.18.0.0/16"/>
 					<accept/>
 				</rule>
 				</zone>',
-		match => "^</zone>.*$",		
+		match => '^<service name="ssh"/></zone>.*$',		
 	}
 	
 	
