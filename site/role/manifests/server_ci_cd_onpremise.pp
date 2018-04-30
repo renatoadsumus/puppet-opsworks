5b8f476e56f91d2dca8f7e97f01848b1578574de
renatoadsumus <renatoadsumus@gmail.com>
2018-04-30 17:27:19 -0300

    Alterando caminho defaul docker para OPT

--- site/role/manifests/server_ci_cd_onpremise.pp
+++ site/role/manifests/server_ci_cd_onpremise.pp
@@ -10,9 +10,7 @@ class role::server_ci_cd_onpremise {
 	
 	file_line {"/etc/firewalld/zones/public_copia.xml":
 		path => "/etc/firewalld/zones/public_copia.xml",
-		line => '<rule family="ipv4"><source address="172.18.0.0/16"/>'
-		+'<accept/>'
-		+'</rule></zone>',
+		line => '<rule family="ipv4"><source address="172.18.0.0/16"/>\n<accept/>\n</rule></zone>',
 		match => "^</zone>.*$",		
 	}
 	
