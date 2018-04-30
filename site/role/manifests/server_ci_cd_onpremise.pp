c2031d2b2a5e21ed44946f688efe88c05753a710
renatoadsumus <renatoadsumus@gmail.com>
2018-04-30 10:55:34 -0300

    Including host in docker and volume DB, Artifacts

--- site/role/manifests/server_ci_cd_onpremise.pp
+++ site/role/manifests/server_ci_cd_onpremise.pp
@@ -2,17 +2,13 @@ class role::server_ci_cd_onpremise {
 	include profile::server_ci_cd_onpremise
 	include profile::agent_gocd_onpremise
 	
-	 file {'creating_db_folder':
+	file {'creating_db_and_artifacts_folder':
         ensure => 'directory',
-        path => '/opt/gocd_server/db',
+        path => [	'/opt/gocd_server/',
+					'/opt/gocd_server/db/',
+					'/opt/gocd_server/artifacts/',],
 		#owner  => "go",
 		#group  => "go",		
 	}
 	
-	file {'creating_artifacts_folder':
-        ensure => 'directory',
-        path => '/opt/gocd_server/artifacts',
-		#owner  => "go",
-		#group  => "go",		
-	}
 }
