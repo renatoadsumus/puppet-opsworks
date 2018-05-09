3775195153266775ded8700c4804d0f705152070
renatoadsumus <renatoadsumus@gmail.com>
2018-05-09 14:45:38 -0300

    Criando volume para pipelines Docker e HOST

--- site/profile/manifests/agent_gocd_onpremise.pp
+++ site/profile/manifests/agent_gocd_onpremise.pp
@@ -5,10 +5,17 @@ class profile::agent_gocd_onpremise inherits profile::base{
 		image_tag => 'latest',		
 	}  
 
+	### CRIANDO AS PASTAS PIPELINE PARA GO-AGENT
+  file {[	'/var/lib/go-agent',
+			'/var/lib/go-agent/pipelines/',]:
+        ensure => 'directory',					
+	}
 	
 	::docker::run { 'gocd_agent':
 		image   => 'renatoadsumus/gocd_agent',		
-		volumes => ['/var/run/docker.sock:/var/run/docker.sock'],
+		volumes => ['/var/run/docker.sock:/var/run/docker.sock','/var/lib/go-agent/pipelines/:/var/lib/go-agent/pipelines/'],
 		require => Docker::Image['renatoadsumus/gocd_agent'],		
 	}	 
+	
+ 
 }
