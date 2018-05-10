f8142171dde0d66ca8a5ac1322182ce794c7fea9
renatoadsumus <renatoadsumus@gmail.com>
2018-05-10 12:55:34 -0300

    Including new user and group go in provisioning

--- site/profile/manifests/agent_gocd_onpremise.pp
+++ site/profile/manifests/agent_gocd_onpremise.pp
@@ -1,5 +1,25 @@
 class profile::agent_gocd_onpremise inherits profile::base{		
 	
+	group { 'go':
+  			  ensure => 'present',
+  			  gid    => '998',
+     }
+
+	 group { 'suporte':
+  			  ensure => 'present',
+  			  gid    => '4000',
+     }
+	 
+	user { 'go':
+		  ensure  => 'present',
+		  comment => 'GO User',		 
+		  groups  => ['go','suporte'],
+		  home    => '/home/go',
+		  shell   => '/bin/bash',
+		  uid     => '999',
+	}
+
+
 	::docker::image { 'renatoadsumus/gocd_agent':	
 		ensure    => 'present',
 		image_tag => 'latest',		
@@ -8,7 +28,7 @@ class profile::agent_gocd_onpremise inherits profile::base{
 	### CRIANDO AS PASTAS PIPELINE PARA GO-AGENT
   file {[	'/opt/gocd_agent/',
 			'/opt/gocd_agent/pipelines/',]:
-        ensure => 'directory',					
+        ensure => 'directory',				
 	}
 	
 	::docker::run { 'gocd_agent':
