b0e2e8844ddf0faeac5693abb67363fe53f364e8
renatoadsumus <renatoadsumus@gmail.com>
2018-05-14 15:16:11 -0300

    Including code for provisioning Dokcer and Container

--- site/profile/manifests/agent_cd.pp
+++ site/profile/manifests/agent_cd.pp
@@ -2,14 +2,14 @@ class profile::agent_cd {
 
 ### CRIANDO PASTA PARA ARMAZENAR BIBLIOTECAS MAVEN - EVITANDO QUE BAIXAR A TODA EXECUÇAO DE UM CONTAINER
   file {[	'/opt/maven/',
-					'/opt/maven/repository/',
+			'/opt/maven/repository/',
 					]:
         ensure => 'directory',					
 	}
 	
 ### CRIANDO PASTA PARA ARMAZENAR OS ARTEFATOS GERADOS E SER UM VOLUME PARA DinD
   file {[	'/opt/gocd_agent/',
-					'/opt/gocd_agent/pipelines',
+			'/opt/gocd_agent/pipelines',
 					]:
         ensure => 'directory',					
 	}
--- site/profile/manifests/run_agent_cd_onpremise.pp
+++ site/profile/manifests/run_agent_cd_onpremise.pp
@@ -8,7 +8,7 @@ class profile::run_agent_cd_onpremise {
 	
 	::docker::run { 'gocd_agent':
 		image   => 'renatoadsumus/gocd_agent',		
-		volumes => ['/var/run/docker.sock:/var/run/docker.sock','/opt/gocd_agent/pipelines:/var/lib/go-agent/pipelines/','/opt/maven/repository/:/var/run/docker.sock'],
+		volumes => ['/var/run/docker.sock:/var/run/docker.sock','/opt/gocd_agent/pipelines:/var/lib/go-agent/pipelines/'],
 		require => Docker::Image['renatoadsumus/gocd_agent'],		
 	}	
 
