0b954380419fbfa030e508f81194dc306cf2a05a
renatoadsumus <renatoadsumus@gmail.com>
2018-05-14 17:21:08 -0300

    Changing Go_CD version to 18.05

--- site/profile/manifests/run_server_cd_onpremise.pp
+++ site/profile/manifests/run_server_cd_onpremise.pp
@@ -17,7 +17,7 @@ class profile::run_server_cd_onpremise {
 	
 	::docker::image { 'renatoadsumus/gocd_server':	
 		ensure    => 'present',
-		image_tag => 'latest',		
+		image_tag => '18.3.0_0509d6bb3d',		
 	}  
 
 
@@ -27,7 +27,7 @@ class profile::run_server_cd_onpremise {
 	} 
 	
 	::docker::run { 'gocd_server':
-		image   => 'renatoadsumus/gocd_server:latest',
+		image   => 'renatoadsumus/gocd_server:18.3.0_0509d6bb3d',
 		ports   => ['8153:8153','8154:8154'],
 		volumes => ['/opt/gocd_server/artifacts/:/var/lib/go-server/artifacts', '/opt/gocd_server/db/:/var/lib/go-server/db', '/opt/gocd_server/cruise-config.xml/:/etc/go/cruise-config.xml'],
 		require => Docker::Image['renatoadsumus/gocd_server'],
