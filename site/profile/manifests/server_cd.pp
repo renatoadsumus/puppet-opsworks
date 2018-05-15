696449a259eeacb958d20fb62ce2b7274e2ddab5
renatoadsumus <renatoadsumus@gmail.com>
2018-05-15 09:30:08 -0300

    Create a new Role AWS to install Docker

--- site/profile/manifests/server_cd.pp
+++ site/profile/manifests/server_cd.pp
@@ -9,22 +9,22 @@ class profile::server_cd {
 	}
 	
 	### RESOLVE O PROBLEMA DOCKER - NO ROUTE TO HOST 
-	file {'/etc/firewalld/zones/public.xml':
-			ensure => 'file',
+	#file {'/etc/firewalld/zones/public.xml':
+			#ensure => 'file',
 			path => '/etc/firewalld/zones/public.xml',
-			content => '<?xml version="1.0" encoding="utf-8"?>
-<zone>
-<short>Public</short>
-<description>For use in public areas. You do not trust the other computers on networks to not harm your computer. Only selected incoming connections are accepted.</description>
-	<service name="dhcpv6-client"/>
-	<service name="ssh"/>
-		</zone>
-		<rule family="ipv4">
-			<source address="172.18.0.0/16"/>
-			<accept/>
-		</rule>
-</zone>',  
+			#content => '<?xml version="1.0" encoding="utf-8"?>
+#<zone>
+#<short>Public</short>
+#<description>For use in public areas. You do not trust the other computers on networks to not harm your computer. Only #selected incoming connections are accepted.</description>
+	#<service name="dhcpv6-client"/>
+	#<service name="ssh"/>
+		#</zone>
+		#<rule family="ipv4">
+			#<source address="172.18.0.0/16"/>
+			#<accept/>
+		#</rule>
+#</zone>',  
 			
-		}	
+		#}	
 
 }
