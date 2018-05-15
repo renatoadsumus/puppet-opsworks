e3841c84da7a0020ebd6d2e41fcad2eade99fdc1
renatoadsumus <renatoadsumus@gmail.com>
2018-05-15 09:24:13 -0300

    Create a new Role AWS to install Docker

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,9 @@
 node 'infodevops3.ogmaster.local'{
 
 	include role::server_ci_cd::onpremise
+}
 
+node 'default'{
+
+	include role::server_ci_cd::aws
 }
--- site/role/manifests/server_ci_cd.pp
+++ site/role/manifests/server_ci_cd.pp
@@ -16,3 +16,11 @@ include profile::run_server_cd_onpremise
 include profile::run_agent_cd_onpremise
 
 }
+
+
+class role::server_ci_cd::aws inherits role::server_ci_cd {
+
+include profile::run_server_cd_aws
+#include profile::run_agent_cd_aws
+
+}
