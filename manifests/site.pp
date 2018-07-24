4d585a3c0152bdc06fb95c6c1338366a7d9a8b56
renatoadsumus <renatoadsumus@gmail.com>
2018-07-24 16:59:44 -0300

    Testando provisionando BASE profile

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,19 +1,5 @@
-node 'infodevops1.ogmaster.local' {		
-		
-	include role::server_ci::onpremise
-}
 
-node 'infodevops2.ogmaster.local' {  
-              		
-	include role::agent_ci_cd::onpremise::install		
-}
-
-node 'infodevops3.ogmaster.local'{
-
-	include role::server_ci_cd::onpremise::run
-}
-
-node 'devops-ugzwejbgqd3dda6c.us-east-1.opsworks-cm.io'{
+node devops-ugzwejbgqd3dda6c.us-east-1.opsworks-cm.io{
 
 	### CRIANDO PASTA PARA SALVAR OS DADOS DO MONGODB EM VOLUME	
 		file{ '/opt/mongo/data_db':
