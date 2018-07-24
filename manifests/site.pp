9f8f7e581ed687b58ad1b22663ef0adf5eff8bdc
renatoadsumus <renatoadsumus@gmail.com>
2018-07-24 16:32:12 -0300

    Testando provisionando BASE profile

--- manifests/site.pp
+++ manifests/site.pp
@@ -15,6 +15,9 @@ node 'infodevops3.ogmaster.local'{
 
 node 'localhost'{
 
-	include java::java8
+	### CRIANDO PASTA PARA SALVAR OS DADOS DO MONGODB EM VOLUME	
+		file{ '/opt/mongo/data_db':
+        ensure  => 'directory',        
+    }
 	#include role::server_ci_cd::aws::run
 }
