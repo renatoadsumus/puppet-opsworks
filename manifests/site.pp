398d4881db31be3e80ba7f46f734d53edc18b34d
renatoadsumus <renatoadsumus@gmail.com>
2018-07-25 10:14:38 -0300

    Testando Opswroks

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,21 @@
-#ip-10-30-20-70.ec2.internal
+node 'infodevops1.ogmaster.local' {		
+		
+	include role::server_ci::onpremise
+}
+
+node 'infodevops2.ogmaster.local' {  
+              		
+	#include role::agent_ci_cd::onpremise::install		
+	include role::server_ci_cd::onpremise::run
+}
+
+node 'infodevops3.ogmaster.local'{
+
+	include role::server_ci_cd::onpremise::run
+}
+
 node 'ip-10-30-20-70.ec2.internal'{
 
-	### CRIANDO PASTA PARA SALVAR OS DADOS DO MONGODB EM VOLUME	
-		file{ '/opt/mongo/':
-        ensure  => 'directory',        
-    }
-	#include role::server_ci_cd::aws::run
+	include role::server_ci_cd::aws::run
 }
+
